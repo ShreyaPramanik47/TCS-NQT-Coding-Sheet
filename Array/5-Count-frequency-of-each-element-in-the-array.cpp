738f22d7e84a3d23e1d39708e858414e913be04b
ShreyaPramanik47 <shreyapramanik909@gmail.com>
2024-10-04 23:48:06 +0530

    Update frequency count logic in array code

--- Array/5-Count-frequency-of-each-element-in-the-array.cpp
+++ Array/5-Count-frequency-of-each-element-in-the-array.cpp
@@ -1,12 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 int cntfreq(int arr[], int n){
-    unordered_map<int, int>mpp;
+    // unordered_map<int, int>mpp;
+    // for(int i=0; i<n; i++){
+    //    mpp[arr[i]]++;
+    // }
+    // for(auto it:mpp){
+    //     cout<<it.first <<" "<< it.second<<endl;
+    // }
+
+// * ANOTHER APPROACH -->
+    vector<bool>track(n,false);
     for(int i=0; i<n; i++){
-       mpp[arr[i]]++;
-    }
-    for(auto it:mpp){
-        cout<<it.first <<" "<< it.second<<endl;
+        if(track[i]==true)continue;
+        int cnt=1;
+        for(int j=i+1; j<n; j++){
+            if(arr[i]==arr[j]){
+             track[j]=true;
+             cnt++;
+            }
+        }
+        cout<< arr[i] <<"-" << cnt<<endl;
     }
 }
 int main(){
